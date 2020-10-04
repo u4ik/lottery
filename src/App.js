3e68815f1995838abb00d68ad5a7ceb4b3765b09
u4ik <affirmativeamit@gmail.com>
2020-10-04 11:47:56 -0400

    fixed occurence count 1-5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-
 import { Scrollbars } from 'react-custom-scrollbars';
-
 import logo from './assets/megamillogo.svg'
-
 import './App.css';
 import Generator from './components/Generator'
-
-
-
-
 function App() {
-
   const appStyle = {
     // backgroundImage: `url(${backgroundImg})`,
     background: 'linear-gradient(0deg, royalblue, blue, darkblue ,navy, cyan 99%,lightblue )',
@@ -20,7 +12,6 @@ function App() {
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     width: '100%'
-
   }
   return (
     <Scrollbars style={{ width: '100vw', height: '100vh' }}>
@@ -30,11 +21,9 @@ function App() {
           <img alt="MegaMillions Logo" src={logo} style={{ width: '30%', filter:'drop-shadow(4px 4px 2px white)',     backgroundColor: 'rgba(0, 45, 125, .1)',paddingLeft:'30%',paddingRight:'30%', paddingTop: '1%', paddingBottom: '1%' ,borderRadius: '3%'}} />
         </div>
         <Generator />
- 
       </div>
     </div>
     </Scrollbars>
   );
 }
-
 export default App;
