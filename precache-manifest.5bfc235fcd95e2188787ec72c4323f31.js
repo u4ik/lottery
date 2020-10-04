d76589d4d69ccd275fa7afb6fec124a53ff14ef9
u4ik <affirmativeamit@gmail.com>
2020-10-04 12:15:03 -0400

    Updates

---
+++ precache-manifest.5bfc235fcd95e2188787ec72c4323f31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b85a9a122c94040c6b15073059687f9",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "d9203eb42c96019d942b",
+    "url": "/lottery/static/css/main.275b7f53.chunk.css"
+  },
+  {
+    "revision": "196749bcb810cc41edd5",
+    "url": "/lottery/static/js/2.510c0242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery/static/js/2.510c0242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9203eb42c96019d942b",
+    "url": "/lottery/static/js/main.99f3c344.chunk.js"
+  },
+  {
+    "revision": "3c6c463699d790bdbf8a",
+    "url": "/lottery/static/js/runtime-main.616fde27.js"
+  },
+  {
+    "revision": "bb7b8679af179138f4eb384180ffba7c",
+    "url": "/lottery/static/media/megamillogo.bb7b8679.svg"
+  }
+]);
